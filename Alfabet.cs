5ad35abd9f80f35ef84c12a05a84593a6b34ccfb
Szymonruminski <56362557+Szymonruminski@users.noreply.github.com>
2019-10-17 14:23:44 +0200

    Add files via upload

---
+++ Alfabet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            for (char znak = 'A'; znak <= 'Z'; znak++)
+            {
+                if (znak != 'Z')
+
+                    Console.Write("{0},", znak);
+                else
+                    Console.Write("{0}.", znak);
+            }
+
+            Console.WriteLine();
+
+            for(char znak2 = 'Z'; znak2 >= 'A'; znak2--)
+            {
+                if (znak2 != 'Z')
+
+                    Console.Write("{0},", znak2);
+                else
+                    Console.Write("{0}.", znak2);
+            }
+
+        }
+    }
+}

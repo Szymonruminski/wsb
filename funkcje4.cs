60d899f70aa7e15b4323834a70b5874b1f5836a2
Szymonruminski <56362557+Szymonruminski@users.noreply.github.com>
2019-11-14 14:38:01 +0100

    Add files via upload

---
+++ funkcje4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int[] tab = new int[2];
+            tab[0] = 5;
+            tab[1] = 10;
+
+
+            Console.WriteLine("Wartości funkcji przed wywołaniem funkcji: {0} i {1}", tab[0], tab[1]);
+            tab1(tab);
+            Console.WriteLine("Wartości funkcji po wywołaniem funkcji: {0} i {1}", tab[0], tab[1]);
+            ShowTab(tab);
+            Console.ReadKey();
+        }
+
+        static void tab1(int[] tab)
+        {
+     
+            tab[0] = 50;
+            tab[1] = 100;
+
+            Console.WriteLine("Wartości funkcji wewnątrz funkcji: {0} i {1}", tab[0], tab[1]);
+        }
+     
+static void ShowTab(int[] tab)
+        {
+            Console.Write("Wartości w tablicy: ");
+            foreach (int item in tab)
+            {
+
+                Console.Write("{0} ", item); ;
+
+            }
+
+        }  
+    
+    }
+
+
+
+}

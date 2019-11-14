e4a84c1785eb725546036bc3a15e9c40d5db1fc3
Szymonruminski <56362557+Szymonruminski@users.noreply.github.com>
2019-11-14 14:29:41 +0100

    Add files via upload

---
+++ funkcje3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a;
+            increment(out a);
+            Console.WriteLine("Wartość zmiennej a po wywołaniem funkcji: {0}", a);
+
+
+            Console.ReadKey();
+        }
+    static void increment(out int x)
+        {
+            x = 1;
+            x += 10;
+            Console.WriteLine("Zmienna x wewnątrz funkcji: {0}", x);
+
+
+        }
+    
+    }
+}

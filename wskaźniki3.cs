fe0b3aaae244bba0f9573fe742023ae0737db6c0
Szymonruminski <56362557+Szymonruminski@users.noreply.github.com>
2019-11-28 14:21:26 +0100

    Add files via upload

---
+++ wskaźniki3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+       unsafe static void Main(string[] args)
+        {
+
+            int[] tab = { 10, 20, 30, 40, 50 };
+
+
+            fixed (int* x = tab) 
+
+                    for(int i = 0; i < tab.Length; i++)
+                {
+
+                    Console.WriteLine("Adres tab[0] = {1}", i, (int)x + i);
+                    Console.WriteLine("Wartość, {0}", *(x+ 1));
+              
+                    Console.ReadKey();
+                }
+        }
+    }
+}
